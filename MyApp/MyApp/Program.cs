7befeaee871445eec3b739757158f4069d5c3f40
Chao Xu <cx525v@gmail.com>
2020-01-31 11:02:23 -0500

    Handle invalid parameters

--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -6,13 +6,20 @@ namespace MyApp
     class Program
     {
        static void Main(string[] args)
-        {               
-            string title = args[0];
-            string catrgoty = args[1];
-            string propertyName = args[2];
-            FilmService fsrv = new FilmService();
-            var task = Task.Run(async() => await fsrv.Perform(title, catrgoty, propertyName));              
-            task.Wait();
+        {
+            if (args != null && args.Length == 3)
+            {
+                string title = args[0];
+                string catrgoty = args[1];
+                string propertyName = args[2];
+                FilmService fsrv = new FilmService();
+                var task = Task.Run(async () => await fsrv.Perform(title, catrgoty, propertyName));
+                task.Wait();
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid arguments");
+            }
         }
     }
 }
